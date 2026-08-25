b386a21694b0664eb2d98a2d8cf9d2608f4a3bfc
Aditya Jagtap <adityajagtap976@gmail.com>
2026-08-25 18:35:35 +0530

    Added maximum 5 attempts

--- Secret Number.py
+++ Secret Number.py
@@ -1,13 +1,14 @@
 import random #This library gives us tools to generate unpredictible numbers.
 
 secret_number = random.randint(1, 20)
+max_attempts = 5
 attempts = 0
 
 print("Welcome to the Secret Number Guessing Game!")
-print("I am thinking of a number between 1 and 20.")
+print(f"I am thinking of a number between 1 and 20. You have {max_attempts} attempts!")
 
 while True:
-  guess = int(input("Take a guess: "))
+  guess = int(input(f"Attempt {attempts + 1}/{max_attempts} - Take a guess: "))
   attempts += 1
 
   if guess < secret_number:
@@ -17,3 +18,5 @@ while True:
   else:
     print(f"Congratulations! You guessed it in {attempts} attempts!")
     break
+else:
+  print(f"Game over! You ran out of attempts. The secret number was {secret_number}.")
